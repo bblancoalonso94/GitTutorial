3c4b7fed85ca422ef8ae8385f2ddaa46ff338096
bblancoalonso94 <92885338+bblancoalonso94@users.noreply.github.com>
2023-12-11 18:56:26 +0100

    happy face

--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Base_Theme_SplashScreen)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //:)
+
 
     }
 }
